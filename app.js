511e3e7ef15b4d86d60def6aacb6af3ac037f197
rukonit <rukon-it@live.com>
2021-10-18 23:41:48 -0400

    Fully functional project

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const User = require('./models/user')
 const userRoutes = require('./routes/users')
+const sanitize = require('express-mongo-sanitize');
+const helmet = require('helmet')
+const MongoStore = require('connect-mongo')(session);
+// const dbUrl = process.env.DB_URL;
 
 
 app.use(express.urlencoded({extended: true}));
@@ -25,12 +29,63 @@ app.use(methodOverride('_method'));
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(flash());
+app.use(helmet({contentSecurityPolicy: false}));
+// app.use(sanitize)
+// const scriptSrcUrls = [
+//     "https://stackpath.bootstrapcdn.com",
+//     "https://api.tiles.mapbox.com",
+//     "https://api.mapbox.com",
+//     "https://kit.fontawesome.com",
+//     "https://cdnjs.cloudflare.com",
+//     "https://cdn.jsdelivr.net",
+// ];
+// const styleSrcUrls = [
+//     "https://kit-free.fontawesome.com",
+//     "https://stackpath.bootstrapcdn.com",
+//     "https://api.mapbox.com",
+//     "https://api.tiles.mapbox.com",
+//     "https://fonts.googleapis.com",
+//     "https://use.fontawesome.com",
+// ];
+// const connectSrcUrls = [
+//     "https://api.mapbox.com",
+//     "https://*.tiles.mapbox.com",
+//     "https://events.mapbox.com",
+// ];
+// const fontSrcUrls = [];
+// app.use(
+//     helmet.contentSecurityPolicy({
+//         directives: {
+//             defaultSrc: [],
+//             connectSrc: ["'self'", ...connectSrcUrls],
+//             scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+//             styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+//             workerSrc: ["'self'", "blob:"],
+//             childSrc: ["blob:"],
+//             objectSrc: [],
+//             imgSrc: [
+//                 "'self'",
+//                 "blob:",
+//                 "data:",
+//                 "https://res.cloudinary.com/douqbebwk/",
+//                 "https://images.unsplash.com",
+//             ],
+//             fontSrc: ["'self'", ...fontSrcUrls],
+//         },
+//     })
+// );
+
+const store = new MongoStore({
+    url: 'mongodb://localhost:27017/yelp-camp',
+    secret: 'yelpcamp'
+})
 
 const sessionConfig = {secret: 'yelpcamp', 
                         resave: false, 
                         saveUninitialized: true,
                         cookie: {
                             httpOnly: true,
+                            // secure: true,
                             expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
                             maxAge: 1000 * 60 * 60 * 24 * 7
                         }
@@ -51,7 +106,7 @@ app.use((req, res, next)=> {
 })
 
 app.engine('ejs', ejsMate);
-
+//'mongodb://localhost:27017/yelp-camp'
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {useNewUrlParser: true,
 useUnifiedTopology: true
 }
@@ -67,7 +122,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res)=> {
-    res.redirect('/campgrounds')
+    res.render('home')
 })
 
 app.use("/campgrounds", campgroundRoutes)
